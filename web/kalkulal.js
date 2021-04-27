15654144d9ecfcf48480b3294ec4f8d7de582b8e
Dajka Dzsenifer <dajkajenifer1@gmail.com>
2021-04-27 11:48:51 +0200

    9.b. A lenyíló lista eseteire készítse el a számolást végző kódrészletet! A fizetendő összeg a szállás árának (fő/éj), a létszámnak és az eltöltött éjszakáknak a szorzata.

--- web/kalkulal.js
+++ web/kalkulal.js
@@ -1,13 +1,13 @@
 function szamol() {
     var letszam = document.getElementById("letszam").value;
-    var ejszaka = document.getElementById("letszam").value;
-    var orszag = document.getElementById("letszam").value;
+    var ejszaka = document.getElementById("ejszaka").value;
+    var orszag = document.getElementById("orszag").value;
     var fizetendo;
     var ar = {};
-    ar ['b'] = 5600;
-    ar ['g'] = 6200;
-    ar ['h'] = 5000;
-    ar ['t'] = 5800;
+    ar['b'] = 5600;
+    ar['g'] = 6200;
+    ar['h'] = 5000;
+    ar['t'] = 5800;
     //--számolás--
       fizetendo = letszam * ejszaka * ar[orszag];
     //------------
